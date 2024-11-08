c3dac6f3567d3fea1cc31757884bd29df1b0eb7c
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-11-08 12:10:10 +0700

    Create profilelembaga.php

---
+++ app/app/view/home/profilelembaga.php
@@ -0,0 +1,349 @@
+<main id="main" class="main">
+
+<div class="pagetitle">
+  <h1>Profile</h1>
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+      <li class="breadcrumb-item active">Profile</li>
+    </ol>
+  </nav>
+</div><!-- End Page Title -->
+
+<section class="section profile">
+  <div class="row">
+    <div class="col-xl-4">
+
+      <div class="card">
+        <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
+        <?php 
+            if($data['gender'] == 'female'){ 
+                $img_profile = "profile-female.jpg";
+            }
+            
+            if($data['gender'] == 'male'){ 
+                $img_profile = "profile-male.jpg";
+            }
+        
+        ?>
+          <img src="<?php echo APP_PATH; ?>/src/assets/img/<?php echo $img_profile; ?>" alt="Profile" class="rounded-circle">
+          <h2><?php echo $data['user_name']; ?></h2>
+          <h3><?php echo $data['email']; ?></h3>
+          <div class="social-links mt-2">
+            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="col-xl-8">
+
+      <div class="card">
+        <div class="card-body pt-3">
+          <!-- Bordered Tabs -->
+          <ul class="nav nav-tabs nav-tabs-bordered">
+
+            <li class="nav-item">
+              <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#profile-overview">Overview</button>
+            </li>
+
+            <li class="nav-item">
+              <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-edit">Edit Profile</button>
+            </li>
+
+            <!-- <li class="nav-item">
+              <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-settings">Settings</button>
+            </li> -->
+
+            <li class="nav-item">
+              <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-change-password">Change Password</button>
+            </li>
+
+          </ul>
+          <div class="tab-content pt-2">
+
+            <div class="tab-pane fade show active profile-overview" id="profile-overview">
+
+              <!-- <h5 class="card-title">Profile Details</h5> -->
+
+              <div class="row">
+                <div class="col-lg-3 col-md-4 label ">Full Name</div>
+                <div class="col-lg-9 col-md-8"><?php echo $data['user_name']; ?></div>
+              </div>
+
+              <div class="row">
+                <div class="col-lg-3 col-md-4 label">Role</div>
+                <div class="col-lg-9 col-md-8"><?php echo $data['role']; ?></div>
+              </div>
+
+              <div class="row">
+                <div class="col-lg-3 col-md-4 label">Jenis Kelamin</div>
+                <div class="col-lg-9 col-md-8"><?php echo $data['gender']; ?></div>
+              </div>
+
+              <div class="row">
+                <div class="col-lg-3 col-md-4 label">Nomor WhatsApp</div>
+                <div class="col-lg-9 col-md-8"><?php echo $data['whatsapp_number']; ?></div>
+              </div>
+
+              <div class="row">
+                <div class="col-lg-3 col-md-4 label">Email</div>
+                <div class="col-lg-9 col-md-8"><?php echo $data['email']; ?></div>
+              </div>
+
+              <!-- <div class="row">
+                <div class="col-lg-3 col-md-4 label">Phone</div>
+                <div class="col-lg-9 col-md-8">(436) 486-3538 x29071</div>
+              </div>
+
+              <div class="row">
+                <div class="col-lg-3 col-md-4 label">Email</div>
+                <div class="col-lg-9 col-md-8">k.anderson@example.com</div>
+              </div> -->
+
+            </div>
+
+            <div class="tab-pane fade profile-edit pt-3" id="profile-edit">
+
+              <!-- Profile Edit Form -->
+              <form>
+                <!-- <div class="row mb-3">
+                  <label for="profileImage" class="col-md-4 col-lg-3 col-form-label">Profile Image</label>
+                  <div class="col-md-8 col-lg-9">
+                    <img src="assets/img/profile-img.jpg" alt="Profile">
+                    <div class="pt-2">
+                      <a href="#" class="btn btn-primary btn-sm" title="Upload new profile image"><i class="bi bi-upload"></i></a>
+                      <a href="#" class="btn btn-danger btn-sm" title="Remove my profile image"><i class="bi bi-trash"></i></a>
+                    </div>
+                  </div>
+                </div> -->
+
+                <div class="row mb-3">
+                  <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Full Name</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="fullName" type="text" class="form-control" id="name" value="<?php echo $data['user_name']; ?>">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="company" class="col-md-4 col-lg-3 col-form-label">Role</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="company" type="text" class="form-control" id="role" value="<?php echo $data['role']; ?>">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="Job" class="col-md-4 col-lg-3 col-form-label">Jenis Kelamin</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="job" type="text" class="form-control" id="gender" value="<?php echo $data['gender']; ?>">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="Country" class="col-md-4 col-lg-3 col-form-label">Nomor WhatsApp</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="country" type="text" class="form-control" id="whatsapp" value="<?php echo $data['whatsapp_number']; ?>">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="Address" class="col-md-4 col-lg-3 col-form-label">Email</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="address" type="text" class="form-control" id="email" value="<?php echo $data['email']; ?>">
+                  </div>
+                </div>
+
+                <!-- <div class="row mb-3">
+                  <label for="Phone" class="col-md-4 col-lg-3 col-form-label">Phone</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="phone" type="text" class="form-control" id="Phone" value="(436) 486-3538 x29071">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="Email" class="col-md-4 col-lg-3 col-form-label">Email</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="email" type="email" class="form-control" id="Email" value="k.anderson@example.com">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="Twitter" class="col-md-4 col-lg-3 col-form-label">Twitter Profile</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="twitter" type="text" class="form-control" id="Twitter" value="https://twitter.com/#">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="Facebook" class="col-md-4 col-lg-3 col-form-label">Facebook Profile</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="facebook" type="text" class="form-control" id="Facebook" value="https://facebook.com/#">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="Instagram" class="col-md-4 col-lg-3 col-form-label">Instagram Profile</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="instagram" type="text" class="form-control" id="Instagram" value="https://instagram.com/#">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="Linkedin" class="col-md-4 col-lg-3 col-form-label">Linkedin Profile</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="linkedin" type="text" class="form-control" id="Linkedin" value="https://linkedin.com/#">
+                  </div>
+                </div> -->
+
+                <div class="text-center">
+                  <button type="submit" class="btn btn-primary">Save Changes</button>
+                </div>
+              </form><!-- End Profile Edit Form -->
+
+            </div>
+
+            <div class="tab-pane fade pt-3" id="profile-settings">
+
+              <!-- Settings Form -->
+              <!-- <form>
+
+                <div class="row mb-3">
+                  <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Email Notifications</label>
+                  <div class="col-md-8 col-lg-9">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="changesMade" checked>
+                      <label class="form-check-label" for="changesMade">
+                        Changes made to your account
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="newProducts" checked>
+                      <label class="form-check-label" for="newProducts">
+                        Information on new products and services
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="proOffers">
+                      <label class="form-check-label" for="proOffers">
+                        Marketing and promo offers
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="securityNotify" checked disabled>
+                      <label class="form-check-label" for="securityNotify">
+                        Security alerts
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="text-center">
+                  <button type="submit" class="btn btn-primary">Save Changes</button>
+                </div>
+              </form> -->
+              <!-- End settings Form -->
+
+            </div>
+
+            <div class="tab-pane fade pt-3" id="profile-change-password">
+              <!-- Change Password Form -->
+              <form>
+
+                <div class="row mb-3">
+                  <label for="currentPassword" class="col-md-4 col-lg-3 col-form-label">Current Password</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="password" type="password" class="form-control" id="currentPassword">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="newPassword" class="col-md-4 col-lg-3 col-form-label">New Password</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="newpassword" type="password" class="form-control" id="newPassword">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="renewPassword" class="col-md-4 col-lg-3 col-form-label">Re-enter New Password</label>
+                  <div class="col-md-8 col-lg-9">
+                    <input name="renewpassword" type="password" class="form-control" id="renewPassword">
+                  </div>
+                </div>
+
+                <div class="text-center">
+                  <button type="submit" class="btn btn-primary">Change Password</button>
+                </div>
+              </form><!-- End Change Password Form -->
+
+            </div>
+
+          </div><!-- End Bordered Tabs -->
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+</main><!-- End #main -->
+
+<script>
+    // Function to send the updated data to the backend and disable input fields after saving
+    function saveUpdatedData() {
+    const userId =  document.getElementById('user_id').value;
+    const name = document.getElementById('name').value;
+    const role = document.getElementById('role').value;
+    const gender = document.getElementById('gender').value;
+    const whatsapp = document.getElementById('whatsapp').value;
+    const email = document.getElementById('email').value;
+
+    const data = {
+        user_id: userId,
+        user_name: name,
+        role: role,
+        gender: gender,
+        whatsapp_number: whatsapp,
+        email: email
+    };
+
+    fetch('https://silaben.site/app/public/login/updateUser', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+      .then(result => {
+          if (result.success) {
+              alert('Data berhasil diperbarui');
+              // Disable input fields again after saving
+              const inputs = document.querySelectorAll("#edit-profile input");
+              inputs.forEach(input => {
+                  input.disabled = true;
+              });
+              // Change button back to 'Perbarui Data'
+              const saveButton = document.getElementById('save-button');
+              saveButton.innerText = 'Perbarui Data';
+              saveButton.removeEventListener("click", saveUpdatedData); // Remove the save function
+              saveButton.addEventListener("click", function () {
+                  // Re-enable input fields for editing
+                  inputs.forEach(input => {
+                      input.disabled = false;
+                  });
+                  this.innerText = 'Simpan';
+                  this.removeEventListener("click", arguments.callee); // Remove this listener
+                  this.addEventListener("click", saveUpdatedData); // Re-attach save listener
+              });
+          } else {
+              alert('Gagal memperbarui data');
+          }
+      }).catch(error => {
+          console.error('Error:', error);
+      });
+}
+</script>
